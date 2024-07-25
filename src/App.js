a57afebcb4b58085044b587f0f94b2a7fb7d093a
kea-nelly <kealeboga.serapane24@gmail.com>
2024-07-25 19:43:56 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/kea-nelly">Kealeboga Nelly Serapane </a> and
-        is{" "}
-        <a href="https://github.com/kea-nelly/react-5" target="_blank">
-          open- sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/kea-nelly">Kealeboga Nelly Serapane </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/kea-nelly/react-5"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open- sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
