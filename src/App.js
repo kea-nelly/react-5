64440d56b7fd9934c5a6d2fbb175bbff02dbf4be
kea-nelly <kealeboga.serapane24@gmail.com>
2024-07-25 19:16:25 +0200

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/kea-nelly">Kealeboga Nelly Serapane </a> and
+        is{" "}
+        <a href="https://github.com/kea-nelly/react-5" target="_blank">
+          open- sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
